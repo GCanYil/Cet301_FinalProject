022a9ab340abbf46e064e1d21b2c79f83cc399d4
Güney Can Yılmaz <160347174+GCanYil@users.noreply.github.com>
2025-12-29 16:56:30 +0300

    Added GET methods for all tables

---
+++ Cet301_FinalProject/Services/LocalDBService.cs
@@ -0,0 +1,60 @@
+namespace Cet301_FinalProject.Services;
+using SQLite;
+using Cet301_FinalProject.Models;
+
+public class LocalDBService
+{
+    private SQLiteAsyncConnection _connection;
+
+    public async Task Init()
+    {
+        if (_connection != null)
+        {
+            return;
+        }
+
+        var databasePath = Path.Combine(FileSystem.AppDataDirectory, "Cet301.db");
+        _connection = new SQLiteAsyncConnection(databasePath);
+
+        await _connection.CreateTableAsync<Customer>();
+        await _connection.CreateTableAsync<Product>();
+        await _connection.CreateTableAsync<Order>();
+        await _connection.CreateTableAsync<OrderItem>();
+    }
+
+    public async Task<List<Customer>> GetCustomers()
+    {
+        await Init();
+        return await _connection.Table<Customer>().ToListAsync();
+    }
+
+    public async Task<List<Product>> GetProducts()
+    {
+        await Init();
+        return await _connection.Table<Product>().ToListAsync();
+    }
+
+    public async Task<List<Order>> GetAllOrders()
+    {
+        await Init();
+        return await _connection.Table<Order>().ToListAsync();
+    }
+
+    public async Task<List<Order>> GetOrderByCustomerId(int customerId)
+    {
+        await Init();
+        return await _connection.Table<Order>().Where(x => x.CustomerId == customerId).ToListAsync();
+    }
+
+    public async Task<List<OrderItem>> GetOrderDetail(int orderId)
+    {
+        await Init();
+        return await _connection.Table<OrderItem>().Where(x => x.OrderId == orderId).ToListAsync();
+    }
+
+    public async Task<List<OrderItem>> GetOrderOfProduct(int productId)
+    {
+        await Init();
+        return await _connection.Table<OrderItem>().Where(x => x.ProductId == productId).ToListAsync();
+    }
+}
