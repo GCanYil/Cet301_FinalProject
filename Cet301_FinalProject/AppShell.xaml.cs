047a82385aa0d269f8cbd7c4f6c117c43eefaa65
Güney Can Yılmaz <160347174+GCanYil@users.noreply.github.com>
2025-12-29 15:29:41 +0300

    Initial Commit

---
+++ Cet301_FinalProject/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace Cet301_FinalProject;
+
+public partial class AppShell : Shell
+{
+    public AppShell()
+    {
+        InitializeComponent();
+    }
+}
---
+++ Cet301_FinalProject/GlobalXmlns.cs
@@ -0,0 +1,2 @@
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "Cet301_FinalProject")]
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "Cet301_FinalProject.Pages")]
