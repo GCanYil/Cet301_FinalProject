38ed35125c31be0346e58affb0a5a4b7c684bc2f
Güney Can Yılmaz <160347174+GCanYil@users.noreply.github.com>
2026-01-05 18:00:23 +0300

    added refreshui logic and added event handler placeholders

--- Cet301_FinalProject/MainPage.xaml.cs
+++ Cet301_FinalProject/MainPage.xaml.cs
@@ -7,5 +7,38 @@ public partial class MainPage : ContentPage
     {
         InitializeComponent();
     }
+
+    //OnAppearing fonksiyonunu AI'dan aldım hocam.
+    protected override void OnAppearing()
+    {
+        base.OnAppearing();
+        RefreshUI();
+    }
+
+    private void RefreshUI()
+    {
+        if (App.IsAdmin)
+        {
+            LBLUserStatus.Text = "User Status: Admin";
+            LBLUserStatus.TextColor = Colors.GreenYellow;
+            LoginPanel.IsVisible = false;
+            AdminPanel.IsVisible = true;
+        }
+        else
+        {
+            LBLUserStatus.Text = "User Status: Standard";
+            LBLUserStatus.TextColor = Colors.Gold;
+            LoginPanel.IsVisible = true;
+            AdminPanel.IsVisible = false;
+        }
+    }
+    
+    private void LoginClicked(object sender, EventArgs e) {}
+    private void LogoutClicked(object sender, EventArgs e) {}
+    
+    private void ToCustomers(object sender, EventArgs e) {}
+    private void ToProducts(object sender, EventArgs e) {}
+    private void ToOrders(object sender, EventArgs e) {}
+    private void ToLogs(object sender, EventArgs e) {}
     
 }
