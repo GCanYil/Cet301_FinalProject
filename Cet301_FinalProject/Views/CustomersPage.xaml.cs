f2a076337741f0c144ecf19036699310fd32d3bd
Güney Can Yılmaz <160347174+GCanYil@users.noreply.github.com>
2026-01-06 13:50:25 +0300

    added function to "adding new customer"

--- Cet301_FinalProject/Views/CustomersPage.xaml.cs
+++ Cet301_FinalProject/Views/CustomersPage.xaml.cs
@@ -3,13 +3,32 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Cet301_FinalProject.Models;
+using Cet301_FinalProject.Services;
 
 namespace Cet301_FinalProject.Views;
 
 public partial class CustomersPage : ContentPage
 {
+    private LocalDBService _dbService = new LocalDBService();
     public CustomersPage()
     {
         InitializeComponent();
+    }
+
+    public async void AddCustomer(object sender, EventArgs e)
+    {
+        var newCustomer = new Customer
+        {
+            Name = EntryCustomerName.Text,
+            Email = EntryCustomerEmail.Text,
+            Address = EntryCustomerAddress.Text,
+            Phone = EntryCustomerPhone.Text,
+        };
+        await _dbService.CreateCustomer(newCustomer);
+        EntryCustomerName.Text = "";
+        EntryCustomerEmail.Text = "";
+        EntryCustomerAddress.Text = "";
+        EntryCustomerPhone.Text = "";
     }
 }
