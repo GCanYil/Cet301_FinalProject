ad176581d4d4bffb09ca69e618c2f4faf5b1b83a
Güney Can Yılmaz <160347174+GCanYil@users.noreply.github.com>
2026-01-06 20:12:02 +0300

    Added confirmorder function

--- Cet301_FinalProject/Views/OrdersPage.xaml.cs
+++ Cet301_FinalProject/Views/OrdersPage.xaml.cs
@@ -3,11 +3,15 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Android.Media;
 using Cet301_FinalProject.Models;
 using Cet301_FinalProject.Services;
 using Cet301_FinalProject.Views;
 
 namespace Cet301_FinalProject.Views;
+
+//Rider foreach'ları otomatik dolduruyor ve VARIABLE değerinin adını değiştirdiğimde hata alıyorum. R
+//Refresh fonksiyonuna özel sandım ama bu dosyada hata alıyorum, o yüzden VARIABLE olarak bıraktım.
 
 public partial class OrdersPage : ContentPage
 {
@@ -79,8 +83,74 @@ public partial class OrdersPage : ContentPage
         _cartList.Remove(item);
         RefreshCart();
     }
-    public void ConfirmOrder(object sender, EventArgs e)
+    public async void ConfirmOrder(object sender, EventArgs e)
     {
+        if (_cartList.Count == 0)
+        {
+            await DisplayAlert("Error", "There are no products in cart", "OK");
+            return;
+        }
+
+        bool stockError = false;
+        foreach (var VARIABLE in _cartList)
+        {
+            var allProducts = await _dbService.GetProducts();
+            var currentProduct = allProducts.FirstOrDefault(product => product.Id == VARIABLE.PId);
+            if (currentProduct.StockAmount < VARIABLE.Amount)
+            {
+                stockError = true;
+                VARIABLE.RowColor = Colors.OrangeRed;
+            }
+            else
+            {
+                VARIABLE.RowColor = Color.FromArgb("#252525");
+            }
+        }
+
+        if (stockError)
+        {
+            RefreshCart();
+            await DisplayAlert("Error", "Not enough items in stock", "OK");
+            return;
+        }
+        else
+        {
+            var selectedCustomer = (Customer)PickerCustomer.SelectedItem;
+            decimal total = 0;
+            foreach (var VARIABLE in _cartList)
+            {
+                total += VARIABLE.TPrice;
+            }
+            var newOrder = new Order
+            {
+                CustomerId = selectedCustomer.Id,
+                Date = DateTime.Now,
+                TotalPrice = total
+            };
+            await _dbService.CreateOrder(newOrder);
+
+            foreach (var VARIABLE in _cartList)
+            {
+                var orderItem = new OrderItem
+                {
+                    OrderId = newOrder.Id,
+                    ProductId = VARIABLE.PId,
+                    Amount = VARIABLE.Amount,
+                    UnitPrice = VARIABLE.Price
+                };
+                await _dbService.CreateOrderItem(orderItem);
+                var allProducts = await _dbService.GetProducts();
+                var currentProduct = allProducts.FirstOrDefault(product => product.Id == VARIABLE.PId);
+                currentProduct.StockAmount -= VARIABLE.Amount;
+                await _dbService.UpdateProduct(currentProduct);
+            }
+        }
+
+        await DisplayAlert("Success", "Order set", "OK");
         
+        _cartList.Clear();
+        PickerCustomer.IsEnabled = true;
+        RefreshCart();
+        PickerCustomer.SelectedItem = null;
     }
 }
